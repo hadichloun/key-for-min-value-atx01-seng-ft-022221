cac002257c766778202ec8dc4b137cd5187aabda
Hadi Chloun <hadi_960@hotmail.com>
2021-02-15 01:19:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,13 @@
 
 
 def key_for_min_value(name_hash)
-
-en
+min_key=nil
+min_value=nil
+name_hash.each do |key, value|
+  if min_value == nil || value < min_value
+    min_key=key
+    min_value=value
+  end
+end
+min_key
+end
